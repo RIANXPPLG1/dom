925194fac26b7549884937aaad5e40437c264407
Indomie <113106114+RIANXPPLG1@users.noreply.github.com>
2023-03-30 15:01:33 +0700

    Add files via upload

---
+++ domjs/dom2.js
@@ -0,0 +1,17 @@
+var email = document.getElementById("email");
+var nama = document.getElementById("nama");
+var username = document.getElementById("username");
+var umur = document.getElementById("umur");
+var pw = document.getElementById("password");
+var kelamin = document.getElementById("gender");
+var kls = document.getElementById("kelas");
+var warna = document.getElementById("warna");
+
+console.log(email)
+console.log(nama)
+console.log(username)
+console.log(umur)
+console.log(pw)
+console.log(kelamin)
+console.log(kls)
+console.log(warna)
